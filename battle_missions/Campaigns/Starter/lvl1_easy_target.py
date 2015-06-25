466e6458c5313b7b43a57e441a48c2385a49dced
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-06-25 08:38:11 +0300

    Change structure

--- battle_missions/Campaigns/Starter/lvl1_easy_target.py
+++ battle_missions/Campaigns/Starter/lvl1_easy_target.py
@@ -1,11 +1,14 @@
 from battle_mocks import *
 
 TIME_LIMIT = 30
+
 MAP_ELEMENTS = [
+    obstacle(tile_position=[0, 0], size=20),
+    obstacle(tile_position=[0, 20], size=20),
+]
+
+DEFENDERS = [
     command_center(level=1, tile_position=[20, 18], player_id=0),
     crystalite_farm(level=1, tile_position=[20, 14], player_id=0),
     crystalite_farm(level=1, tile_position=[20, 123], player_id=0),
-
-    obstacle(tile_position=[0, 0], size=20),
-    obstacle(tile_position=[0, 20], size=20),
 ]
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='EoC Battle Missions',
+    version='0.1',
+    description='Missions for "Empire of Code" Battles',
+    author='Bryukhanov Valentin',
+    author_email='bryukh@checkio.org',
+)
