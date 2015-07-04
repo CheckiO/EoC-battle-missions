0a55d3be2bb2bbc4f4e5d27a6b04c15731219f6f
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-07-04 11:24:57 +0300

    Fix for mission 5 and defender code

--- battle_missions/campaigns/starter/lvl5_rocket_pillars.py
+++ battle_missions/campaigns/starter/lvl5_rocket_pillars.py
@@ -1,7 +1,3 @@
-from battle_missions.utils import template, battle_codes
-
-__author__ = 'bryukh'
-
 from copy import deepcopy
 
 from battle_mocks import *
@@ -25,7 +21,7 @@ MISSION_DATA[TERM.MAP_ELEMENTS] = [
     rocket_tower(level=2, tile_position=[22, 0], player_id=0, code_id=TOWER_CODE_ID),
     rocket_tower(level=2, tile_position=[24, 0], player_id=0, code_id=TOWER_CODE_ID),
     rocket_tower(level=2, tile_position=[20, 38], player_id=0, code_id=TOWER_CODE_ID),
-    rocket_tower(level=2, tile_position=[20, 40], player_id=0, code_id=TOWER_CODE_ID),
-    rocket_tower(level=2, tile_position=[20, 42], player_id=0, code_id=TOWER_CODE_ID),
+    rocket_tower(level=2, tile_position=[22, 38], player_id=0, code_id=TOWER_CODE_ID),
+    rocket_tower(level=2, tile_position=[24, 38], player_id=0, code_id=TOWER_CODE_ID),
 ]
 
