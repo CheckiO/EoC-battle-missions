cd48b2e8b316774d7c5ee432427dd166bf901fc6
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-08-13 09:37:36 +0300

    update sniper guard level

--- battle_missions/campaigns/starter/sniper_guards.py
+++ battle_missions/campaigns/starter/sniper_guards.py
@@ -12,14 +12,13 @@ TOWER_CODE_ID = battle_codes.PY_TOWER_SHOOT_FIRST_CODE[TERM.ID]
 
 MISSION_DATA = deepcopy(template.MISSION_DATA)
 
-MISSION_DATA[TERM.TIME_LIMIT] = 60
+MISSION_DATA[TERM.TIME_LIMIT] = 90
 MISSION_DATA[TERM.MAP_ELEMENTS] = [
     # Nature
     obstacle(tile_position=[0, 0], size=20),
     obstacle(tile_position=[0, 20], size=20),
     # Defenders
-    command_center(level=4, tile_position=[21, 18], player_id=0),
+    command_center(level=5, tile_position=[25, 18], player_id=0),
     sentry_tower(level=1, tile_position=[25, 14], player_id=0, code_id=TOWER_CODE_ID),
     sentry_tower(level=1, tile_position=[25, 23], player_id=0, code_id=TOWER_CODE_ID),
-    sentry_tower(level=1, tile_position=[25, 18], player_id=0, code_id=TOWER_CODE_ID),
 ]
